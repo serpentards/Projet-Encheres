5991c563fbf4f30a439f61d2efbe1b06592a6fe7
Nicolas BACON <Nicolas BACON@DESKTOP-9PISB9P>
2020-04-02 09:49:48 +0200

    oublie de deux trois truc

--- src/fr/eni/encheres/servlets/AfficherArticle.java
+++ src/fr/eni/encheres/servlets/AfficherArticle.java
@@ -2,8 +2,6 @@ package fr.eni.encheres.servlets;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
